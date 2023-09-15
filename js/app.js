317e4326bb549f9fdd79b32d99e75a4bae39497e
nubelz <dolphusthomas@gmail.com>
2023-09-15 04:39:05 -0400

    update readme

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
-
 const cardValues = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
-const cardSuits = ["Hearts", "Diamonds", "Clubs", "Spades"];
+const cardSuits = ["♥", "♦", "♣", "♠"];
 
 let warCards = [];
 let cardDeck = [];
@@ -27,26 +26,6 @@ function shuffleDeck() {
 createDeck();
 shuffleDeck();
 
-
-function drawCard() {
-  if (cardDeck.length === 0) {
-      alert("No more cards in the deck!");
-      return;
-  }
-
-  player1Card = cardDeck.pop();
-  player2Card = cardDeck.pop();
-
-  document.getElementById('card1').textContent = `${player1Card.value} of ${player1Card.suit}`;
-  document.getElementById('card2').textContent = `${player2Card.value} of ${player2Card.suit}`;
-
-  determineWinner();
-
-  if (cardDeck.length === 52) {
-      endGame();
-  }
-}
-
 function drawCard() {
     if (cardDeck.length === 0) {
       
@@ -56,8 +35,8 @@ function drawCard() {
     player1Card = cardDeck.pop();
     player2Card = cardDeck.pop();
 
-    document.getElementById('card1').textContent = `${player1Card.value} of ${player1Card.suit}`;
-    document.getElementById('card2').textContent = `${player2Card.value} of ${player2Card.suit}`;
+    document.getElementById('card1').textContent = `${player1Card.value} ${player1Card.suit}`;
+    document.getElementById('card2').textContent = `${player2Card.value} ${player2Card.suit}`;
 
     determineWinner();
 
@@ -68,13 +47,10 @@ function drawCard() {
 
 function endGame() {
     const resultElement = document.getElementById('result');
-    if (player1Score > player2Score) {
-        resultElement.textContent = 'Player 1 wins the game!';
-    } else if (player2Score > player1Score) {
-        resultElement.textContent = 'Player 2 wins the game!';
+    if (player1Score === 52) {
+        document.getElementById('result').textContent = 'Player 1 wins the game!';
     } else {
-        resultElement.textContent = 'It\'s a tie game! Time for WAR!';
-        startWar();
+        document.getElementById('result').textContent = 'Player 2 wins the game!';
     }
 
     
@@ -95,7 +71,7 @@ function startWar() {
 
     document.getElementById('result').textContent = 'WAR!';
 
-    document.querySelector('button').disabled = true;
+    document.querySelector('button').disabled = false;
 
   
     setTimeout(revealNextWarCard, 1000);
@@ -127,52 +103,42 @@ function revealNextWarCard() {
         return;
     }
 
-
-    document.querySelector('button').disabled = false;
-}
-
-function determineWinner() {
-    const resultElement = document.getElementById('result');
-    if (player1Card.value > player2Card.value) {
-        resultElement.textContent = 'Player 1 wins!';
-    } else if (player2Card.value > player1Card.value) {
-        resultElement.textContent = 'Player 2 wins!';
-    } else {
-        resultElement.textContent = 'It\'s a tie!';
-    }
 }
 
 let player1Score = 0;
 let player2Score = 0;
 
 function updateScoreboard() {
-    document.getElementById('player1Score').textContent = `Player 1: ${player1Score}`;
-    document.getElementById('player2Score').textContent = `Player 2: ${player2Score}`;
+  document.getElementById('player1Score').textContent = `Player 1: ${player1Score}`;
+  document.getElementById('player2Score').textContent = `Player 2: ${player2Score}`;
 }
 
 function determineWinner() {
-    const resultElement = document.getElementById('result');
-    if (player1Card.value > player2Card.value) {
-        resultElement.textContent = 'Player 1 wins this round!';
-        player1Score++;
-    } else if (player2Card.value > player1Card.value) {
-        resultElement.textContent = 'Player 2 wins this round!';
-        player2Score++;
-    } else {
-        resultElement.textContent = 'It\'s a tie for this round!';
-    }
+  if (player1Card.value > player2Card.value) {
+      player1Score++;
+      player2Score--;
+  } else if (player2Card.value > player1Card.value) {
+      player2Score++;
+      player1Score--;
+  } else {
+      startWar();
+      return;
+  }
+
+  if (player1Score < 0) {
+      player1Score = 0;
+  }
+
+  if (player2Score < 0) {
+      player2Score = 0;
+  }
 
 
     updateScoreboard();
 
-    if (player1Score + player2Score === 26) {
-        if (player1Score > player2Score) {
-            resultElement.textContent = 'Player 1 wins the game!';
-        } else if (player2Score > player1Score) {
-            resultElement.textContent = 'Player 2 wins the game!';
-        } else {
-            resultElement.textContent = 'It\'s a tie game!';
-        }
+    if (player1Score >= maxScore || player2Score >= maxScore) {
+        endGame();
+        return;
     }
 }
 
@@ -183,11 +149,11 @@ function newGame() {
   player1Card = null;
   player2Card = null;
 
-  player1Score = 0;
-  player2Score = 0;
+  player1Score = 26;
+  player2Score = 26;
 
-  document.getElementById('card1').textContent = '?';
-  document.getElementById('card2').textContent = '?';
+  document.getElementById('card1').textContent = '';
+  document.getElementById('card2').textContent = '';
 
   document.getElementById('result').textContent = '';
 
